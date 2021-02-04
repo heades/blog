81e9f315a18a7d8730718286bce8d66b0c32513d
Harley Eades <harley.eades@gmail.com>
2021-02-04 12:08:11 -0500

    Updated the definition lua filter to allow a list of blocks within the definition environment.

--- lua-filters/definition.lua
+++ lua-filters/definition.lua
@@ -1,10 +1,20 @@
 function environment(id, body, format, label)
-  if format == "latex" then        
-     return {
-      pandoc.RawBlock('latex', '\\begin{'..id..'}\\label{'..label..'}'),
-      pandoc.Plain(body),
-      pandoc.RawBlock('latex', '\\end{'..id..'}')
-      }
+  content = {}
+  if format == "latex" then
+     -- Create the latex content blocks.
+     --   1. Start the latex environment id with label.
+     content[1] = pandoc.RawBlock('latex', '\\begin{'..id..'}\\label{'..label..'}')
+     --   2. Since body is list of blocks, we need to add each othem to the content table.
+     block_count = 1
+     for i,bi in pairs(body) do
+          content[i+1] = bi
+          block_count = block_count + 1
+     end
+     -- Close the latex environment id with \end{id}.
+     content[block_count+1] = pandoc.RawBlock('latex', '\\end{'..id..'}')
+
+     -- Return out newly formated block.
+     return content
   elseif format == "html" then
       return {
       pandoc.RawBlock('html', '<div class="'..id..'">'),
@@ -21,7 +31,7 @@ function Div(el)
      id == "theorem"    or
      id == "corollary"  then
      local label = el.attributes["label"]
-     local body = el.content[1].content
+     local body = el.content     
      return environment(id, body, FORMAT, label)
   end
 end
